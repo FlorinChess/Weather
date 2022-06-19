758a4fc748a0acd04b6f07d73ca5ee69ab6730fb
Florin Zamfir <florin.chess@gmail.com>
2022-06-19 20:22:25 +0200

    Small performance improvements

--- Weather.Core/ApiCaller.cs
+++ Weather.Core/ApiCaller.cs
@@ -1,21 +1,18 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Weather.Core.Exceptions;
 using Weather.Core.Models;
 
 namespace Weather.Core
 {
-    public class ApiCaller
+    public static class ApiCaller
     {
         public static async Task<WeatherInformation> GetWeather(string location, string? lang = "en")
         {
             using HttpClient httpClient = new HttpClient();
-            var response = await httpClient.GetAsync($"https://api.weatherapi.com/v1/forecast.json?key=592925aed75849868d1112324222302&q={ location }&days=3&aqi=no&alerts=no&lang={ lang }");
+            var response = await httpClient.GetAsync($"https://api.weatherapi.com/v1/forecast.json?key=592925aed75849868d1112324222302&q={location}&days=3&aqi=no&alerts=no&lang={lang}");
 
             if (response.IsSuccessStatusCode!)
             {
