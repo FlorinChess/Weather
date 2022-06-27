1443a633048c56d12a0f6cf10b60b17fd325bdda
Florin Zamfir <florin.chess@gmail.com>
2022-06-27 16:27:43 +0200

    Add helper class for managing app settings

---
+++ Weather.App/Weather.App/Helpers/Settings.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Essentials;
+
+namespace Weather.App.Helpers
+{
+    public static class Settings
+    {
+        const bool _isMetriSystemEnabled = true;
+        public static bool IsMetricSystemEnabled
+        {
+            get => Preferences.Get(nameof(IsMetricSystemEnabled), _isMetriSystemEnabled);
+            set => Preferences.Set(nameof(IsMetricSystemEnabled), value);
+        }
+
+        const string _weatherLocation = "Vienna";
+        public static string WeatherLocation
+        {
+            get => Preferences.Get(nameof(WeatherLocation), _weatherLocation);
+            set => Preferences.Set(nameof(WeatherLocation), value);
+        }
+    }
+}
