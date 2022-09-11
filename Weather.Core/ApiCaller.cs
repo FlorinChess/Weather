6a72637142a59199609b528b6fa84a758565da9d
Florin Zamfir <florin.chess@gmail.com>
2022-09-11 14:05:56 +0200

    Added in-memory caching

--- Weather.Core/ApiCaller.cs
+++ Weather.Core/ApiCaller.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Weather.Core.Exceptions;
 using Weather.Core.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Weather.Core
 {
@@ -14,46 +15,59 @@ namespace Weather.Core
         public const string ErrorOccured = "An error occured! Please try again!";
 
         private readonly HttpClient _httpClient;
+        private readonly IMemoryCache _cache;
 
         public ApiCaller(HttpClient httpClient)
         {
             _httpClient = httpClient;
+            _cache = new MemoryCache(new MemoryCacheOptions());
         }
 
-        public async Task<WeatherInformation> GetWeather(string location, string lang = "en")
+        public async ValueTask<WeatherInformation> GetWeather(string location, string lang = "en")
         {
-            var response = await _httpClient.GetAsync($"https://api.weatherapi.com/v1/forecast.json?key=592925aed75849868d1112324222302&q={location}&days=3&aqi=no&alerts=no&lang={lang}");
-
-            if (response.IsSuccessStatusCode!)
+            if (_cache.TryGetValue(location, out WeatherInformation weatherInformation))
             {
-                // Deserialize content object 
-                string content = await response.Content.ReadAsStringAsync();
-                return JsonSerializer.Deserialize<WeatherInformation>(content);
-            }
-            else if ((int)response.StatusCode == 400)
-            {
-                // Deserialize error object to get error code
-                string content = await response.Content.ReadAsStringAsync();
-                ApiError apiError = JsonSerializer.Deserialize<ApiError>(content);
-
-                // Error message that gets displayed depending on the error code
-                string errorMessage = ErrorOccured;
-
-                if (apiError.error.code == 1006)
-                {
-                    // Invalid weather location
-                    errorMessage = InvalidWeatherLocation;
-                }
-                else if (apiError.error.code == 9999)
-                {
-                    // API internal error
-                    errorMessage = ServerSideError;
-                }
-                throw new LocationNotFoundException(errorMessage);
+                return weatherInformation;
             }
             else
             {
-                throw new Exception(response.StatusCode.ToString());
+                var response = await _httpClient.GetAsync($"https://api.weatherapi.com/v1/forecast.json?key=592925aed75849868d1112324222302&q={location}&days=3&aqi=no&alerts=no&lang={lang}");
+
+                if (response.IsSuccessStatusCode!)
+                {
+                    // Deserialize content object 
+                    string content = await response.Content.ReadAsStringAsync();
+                    var deserializedContent = JsonSerializer.Deserialize<WeatherInformation>(content);
+
+                    _cache.Set(location, deserializedContent, TimeSpan.FromMinutes(15));
+                    return deserializedContent;
+                }
+                else if ((int)response.StatusCode == 400)
+                {
+                    // Deserialize error object to get error code
+                    string content = await response.Content.ReadAsStringAsync();
+                    ApiError apiError = JsonSerializer.Deserialize<ApiError>(content);
+
+                    // Error message that gets displayed depending on the error code
+                    string errorMessage = ErrorOccured;
+
+                    switch (apiError.error.code)
+                    {
+                        case 1006:
+                            // Invalid weather location
+                            errorMessage = InvalidWeatherLocation;
+                            break;
+                        case 9999:
+                            // API internal error
+                            errorMessage = ServerSideError;
+                            break;
+                    }
+                    throw new LocationNotFoundException(errorMessage);
+                }
+                else
+                {
+                    throw new Exception(response.StatusCode.ToString());
+                }
             }
         }
     }
