b1320ab790ca5588f17e6b73b8de94173e19d342
Florin Zamfir <florin.chess@gmail.com>
2022-09-13 22:01:16 +0200

    reworked FeedbackView

--- Weather/ViewModels/FeedbackViewModel.cs
+++ Weather/ViewModels/FeedbackViewModel.cs
@@ -6,36 +6,11 @@ using Weather.Stores;
 
 namespace Weather.ViewModels
 {
-    public class FeedbackViewModel : BaseViewModel
+    public sealed class FeedbackViewModel : BaseViewModel
     {
+        private readonly string KoFiUrl = "https://ko-fi.com/florin_chess";
+        private readonly string FeedbackManagerUrl = "https://feedback-manager.azurewebsites.net";
         private readonly NavigationStore _navigationStore;
-
-        #region Properties
-
-        private string _feedbackMessage;
-        public string FeedbackMessage
-        {
-            get => _feedbackMessage;
-            set
-            {
-                _feedbackMessage = value;
-                FeedbackMessageLength = _feedbackMessage.Length.ToString();
-                OnPropertyChanged();
-            }
-        }
-
-        private string _feedbackMessageLength = "0/300";
-        public string FeedbackMessageLength
-        {
-            get => _feedbackMessageLength;
-            set
-            {
-                _feedbackMessageLength = $"{value}/300";
-                OnPropertyChanged();
-            }
-        }
-
-        #endregion
 
         #region Commands
 
@@ -49,40 +24,38 @@ namespace Weather.ViewModels
         {
             _navigationStore = navigationStore;
 
-            DonateCommand = new RelayCommand(_ =>
-            {
-                // Registry path for Google Chrome
-                var path = Microsoft.Win32.Registry.GetValue(@"HKEY_CLASSES_ROOT\ChromeHTML\shell\open\command", null, null) as string;
+            DonateCommand = new RelayCommand(_ => OpenBrowser(KoFiUrl));
 
-                string url = "https://ko-fi.com/florin_chess";
-
-                if (!string.IsNullOrEmpty(path))
-                {
-                    var split = path.Split('\"');
-                    path = split.Length >= 2 ? split[1] : null;
-
-                    // Starts the process from the given path in Google Chrome
-                    System.Diagnostics.Process.Start(path, url);
-                }
-                else
-                {
-                    // Start the process in Microsoft Edge
-                    // NOTE: this is a workaround; /C to terminate cmd.exe after the command runs
-                    // TODO: find a cleaner way to open a url with Edge
-                    System.Diagnostics.Process.Start("CMD.exe", $"/C start msedge {url}");
-                }
-
-                NavigateHome(serviceProvider);
-            });
+            SubmitFeedbackCommand = new RelayCommand(_ => OpenBrowser(FeedbackManagerUrl));
 
             CloseCommand = new RelayCommand(_ =>  NavigateHome(serviceProvider));
-
-            SubmitFeedbackCommand = new RelayCommand(_ => NavigateHome(serviceProvider));
         }
 
         private void NavigateHome(IServiceProvider serviceProvider)
         {
             _navigationStore.CurrentViewModel = serviceProvider.GetRequiredService<HomeViewModel>();
+        }
+
+        private static void OpenBrowser(string url)
+        {
+            // Registry path for Google Chrome
+            var path = Microsoft.Win32.Registry.GetValue(@"HKEY_CLASSES_ROOT\ChromeHTML\shell\open\command", null, null) as string;
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                var split = path.Split('\"');
+                path = split.Length >= 2 ? split[1] : null;
+
+                // Starts the process from the given path in Google Chrome
+                System.Diagnostics.Process.Start(path, url);
+            }
+            else
+            {
+                // Start the process in Microsoft Edge
+                // NOTE: this is a workaround; /C to terminate cmd.exe after the command runs
+                // TODO: find a cleaner way to open a url with Edge
+                System.Diagnostics.Process.Start("CMD.exe", $"/C start msedge {url}");
+            }
         }
     }
 }
