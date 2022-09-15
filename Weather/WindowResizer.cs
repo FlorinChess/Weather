b72f329546b08860006fa5ab4be0de95230d816a
Florin Zamfir <florin.chess@gmail.com>
2022-09-15 19:14:45 +0200

    simplified visual tree and optimized styles

--- Weather/WindowResizer.cs
+++ Weather/WindowResizer.cs
@@ -35,32 +35,32 @@ namespace Weather
         /// <summary>
         /// The window to handle the resizing for
         /// </summary>
-        private readonly Window mWindow;
+        private readonly Window _window;
 
         /// <summary>
         /// The last calculated available screen size
         /// </summary>
-        private Rect mScreenSize = new Rect();
+        private Rect _screenSize = new();
 
         /// <summary>
         /// How close to the edge the window has to be to be detected as at the edge of the screen
         /// </summary>
-        private readonly int mEdgeTolerance = 2;
+        private readonly ushort _edgeTolerance = 2;
 
         /// <summary>
         /// The transform matrix used to convert WPF sizes to screen pixels
         /// </summary>
-        private Matrix mTransformToDevice;
+        private Matrix _transformToDevice;
 
         /// <summary>
         /// The last screen the window was on
         /// </summary>
-        private IntPtr mLastScreen;
+        private IntPtr _lastScreen;
 
         /// <summary>
         /// The last known dock position
         /// </summary>
-        private WindowDockPosition mLastDock = WindowDockPosition.Undocked;
+        private WindowDockPosition _lastDock = WindowDockPosition.Undocked;
 
         #endregion
 
@@ -96,16 +96,16 @@ namespace Weather
         /// <param name="adjustSize">The callback for the host to adjust the maximum available size if needed</param>
         public WindowResizer(Window window)
         {
-            mWindow = window;
+            _window = window;
 
             // Create transform visual (for converting WPF size to pixel size)
             GetTransform();
 
             // Listen out for source initialized to setup
-            mWindow.SourceInitialized += Window_SourceInitialized;
+            _window.SourceInitialized += Window_SourceInitialized;
 
             // Monitor for edge docking
-            mWindow.SizeChanged += Window_SizeChanged;
+            _window.SizeChanged += Window_SizeChanged;
         }
 
         #endregion
@@ -118,17 +118,17 @@ namespace Weather
         private void GetTransform()
         {
             // Get the visual source
-            var source = PresentationSource.FromVisual(mWindow);
+            var source = PresentationSource.FromVisual(_window);
 
             // Reset the transform to default
-            mTransformToDevice = default(Matrix);
+            _transformToDevice = default(Matrix);
 
             // If we cannot get the source, ignore
             if (source == null)
                 return;
 
             // Otherwise, get the new transform object
-            mTransformToDevice = source.CompositionTarget.TransformToDevice;
+            _transformToDevice = source.CompositionTarget.TransformToDevice;
         }
 
         /// <summary>
@@ -136,10 +136,10 @@ namespace Weather
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Window_SourceInitialized(object sender, System.EventArgs e)
+        private void Window_SourceInitialized(object sender, EventArgs e)
         {
             // Get the handle of this window
-            var handle = (new WindowInteropHelper(mWindow)).Handle;
+            var handle = (new WindowInteropHelper(_window)).Handle;
             var handleSource = HwndSource.FromHwnd(handle);
 
             // If not found, end
@@ -162,27 +162,27 @@ namespace Weather
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             // We cannot find positioning until the window transform has been established
-            if (mTransformToDevice == default(Matrix))
+            if (_transformToDevice == default(Matrix))
                 return;
 
             // Get the WPF size
             var size = e.NewSize;
 
             // Get window rectangle
-            var top = mWindow.Top;
-            var left = mWindow.Left;
+            var top = _window.Top;
+            var left = _window.Left;
             var bottom = top + size.Height;
-            var right = left + mWindow.Width;
+            var right = left + _window.Width;
 
             // Get window position/size in device pixels
-            var windowTopLeft = mTransformToDevice.Transform(new Point(left, top));
-            var windowBottomRight = mTransformToDevice.Transform(new Point(right, bottom));
+            var windowTopLeft = _transformToDevice.Transform(new Point(left, top));
+            var windowBottomRight = _transformToDevice.Transform(new Point(right, bottom));
 
             // Check for edges docked
-            var edgedTop = windowTopLeft.Y <= (mScreenSize.Top + mEdgeTolerance);
-            var edgedLeft = windowTopLeft.X <= (mScreenSize.Left + mEdgeTolerance);
-            var edgedBottom = windowBottomRight.Y >= (mScreenSize.Bottom - mEdgeTolerance);
-            var edgedRight = windowBottomRight.X >= (mScreenSize.Right - mEdgeTolerance);
+            var edgedTop = windowTopLeft.Y <= (_screenSize.Top + _edgeTolerance);
+            var edgedLeft = windowTopLeft.X <= (_screenSize.Left + _edgeTolerance);
+            var edgedBottom = windowBottomRight.Y >= (_screenSize.Bottom - _edgeTolerance);
+            var edgedRight = windowBottomRight.X >= (_screenSize.Right - _edgeTolerance);
 
             // Get docked position
             var dock = WindowDockPosition.Undocked;
@@ -197,12 +197,12 @@ namespace Weather
                 dock = WindowDockPosition.Undocked;
 
             // If dock has changed
-            if (dock != mLastDock)
+            if (dock != _lastDock)
                 // Inform listeners
                 WindowDockChanged(dock);
 
             // Save last dock position
-            mLastDock = dock;
+            _lastDock = dock;
         }
 
         #endregion
@@ -243,7 +243,8 @@ namespace Weather
         private void WmGetMinMaxInfo(IntPtr lParam)
         {
             // Get the point position to determine what screen we are on
-            GetCursorPos(out POINT lMousePosition);
+            POINT lMousePosition;
+            GetCursorPos(out lMousePosition);
 
             // Get the primary monitor at cursor position 0,0
             var lPrimaryScreen = MonitorFromPoint(new POINT(0, 0), MonitorOptions.MONITOR_DEFAULTTOPRIMARY);
@@ -257,11 +258,11 @@ namespace Weather
             var lCurrentScreen = MonitorFromPoint(lMousePosition, MonitorOptions.MONITOR_DEFAULTTONEAREST);
 
             // If this has changed from the last one, update the transform
-            if (lCurrentScreen != mLastScreen || mTransformToDevice == default(Matrix))
+            if (lCurrentScreen != _lastScreen || _transformToDevice == default(Matrix))
                 GetTransform();
 
             // Store last know screen
-            mLastScreen = lCurrentScreen;
+            _lastScreen = lCurrentScreen;
 
             // Get min/max structure to fill with information
             var lMmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
@@ -284,13 +285,13 @@ namespace Weather
             }
 
             // Set min size
-            var minSize = mTransformToDevice.Transform(new Point(mWindow.MinWidth, mWindow.MinHeight));
+            var minSize = _transformToDevice.Transform(new Point(_window.MinWidth, _window.MinHeight));
 
             lMmi.ptMinTrackSize.X = (int)minSize.X;
             lMmi.ptMinTrackSize.Y = (int)minSize.Y;
 
             // Store new size
-            mScreenSize = new Rect(lMmi.ptMaxPosition.X, lMmi.ptMaxPosition.Y, lMmi.ptMaxSize.X, lMmi.ptMaxSize.Y);
+            _screenSize = new Rect(lMmi.ptMaxPosition.X, lMmi.ptMaxPosition.Y, lMmi.ptMaxSize.X, lMmi.ptMaxSize.Y);
 
             // Now we have the max size, allow the host to tweak as needed
             Marshal.StructureToPtr(lMmi, lParam, true);
@@ -306,14 +307,16 @@ namespace Weather
         MONITOR_DEFAULTTONEAREST = 0x00000002
     }
 
+
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-    public class MONITORINFO
+    public sealed class MONITORINFO
     {
         public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
-        public Rectangle rcMonitor = new Rectangle();
-        public Rectangle rcWork = new Rectangle();
+        public Rectangle rcMonitor = new();
+        public Rectangle rcWork = new();
         public int dwFlags = 0;
     }
+
 
     [StructLayout(LayoutKind.Sequential)]
     public struct Rectangle
@@ -322,10 +325,10 @@ namespace Weather
 
         public Rectangle(int left, int top, int right, int bottom)
         {
-            this.Left = left;
-            this.Top = top;
-            this.Right = right;
-            this.Bottom = bottom;
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
         }
     }
 
@@ -356,8 +359,8 @@ namespace Weather
         /// </summary>
         public POINT(int x, int y)
         {
-            this.X = x;
-            this.Y = y;
+            X = x;
+            Y = y;
         }
     }
 
