ce9b5ea4014cca999ba23d2fb08c87c44c188a90
Florin Zamfir <f.zamfir@student.tugraz.at>
2023-08-20 17:05:45 +0200

    added exception classes

---
+++ Weather.Core/Exceptions/InvalidWeatherLocationException.cs
@@ -0,0 +1,7 @@
+﻿namespace Weather.Core.Exceptions
+{
+    public sealed class InvalidWeatherLocationException : WeatherClientException
+    {
+        public override string Message => "Invalid weather location! Please enter a different location!";
+    }
+}
---
+++ Weather.Core/Exceptions/ServerSideIssueException.cs
@@ -0,0 +1,7 @@
+﻿namespace Weather.Core.Exceptions
+{
+    public sealed class ServerSideIssueException : WeatherClientException
+    {
+        public override string Message => "Server-side error! Please try again later!";
+    }
+}
